5872a77765b72f5645f293c24f58f739cc4c4102
Yernar Masujima <Yernar.Massujima@kaspi.kz>
2023-05-07 13:15:44 +0600

    Initial Commit

---
+++ RelatedAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RelatedAnimation
+//
+//  Created by Yernar Masujima on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
